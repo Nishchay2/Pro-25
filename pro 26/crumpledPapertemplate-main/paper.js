f7dccb09198a878775159eff43f7dbb919270ca2
Nishchay2 <76587026+Nishchay2@users.noreply.github.com>
2021-02-17 17:01:19 +0530

    Add files via upload

---
+++ pro 26/crumpledPapertemplate-main/paper.js
@@ -0,0 +1,25 @@
+class paper
+{
+	constructor(x,y){
+
+        var option={
+isStatic:false,
+restitution:0.3,
+friction:0.5,
+density:1.2
+        }
+        this.body=Bodies.circle(x,y,50,option)
+        World.add(world,this.body)
+        this.image=loadImage("paper.png")
+    }
+    display(){
+            var pos=this.body.position
+            var angle=this.body.angle
+            push()
+            translate(pos.x,pos.y)
+            rotate (angle)
+            imageMode(CENTER)
+            image (this.image,0,0,50,50)
+            pop ()
+    }
+}
